4819aa747b033b977bb423f1957d2aa606f4f25a
camb <cameron.bec@gmail.com>
2017-03-25 18:31:50 -0700

    3.88 updated materials and fixed door collision

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -23,7 +23,7 @@ public:
 
 private:
 	// How far ahead of the player can we reach in cm
-	float Reach = 100.f;
+	float Reach = 150.f;
 	
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
 	UInputComponent* InputComponent = nullptr;
